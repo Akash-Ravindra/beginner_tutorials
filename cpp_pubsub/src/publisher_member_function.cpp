a7971729851ad5f481756bfc4055d361d9009419
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-11-16 18:41:09 -0500

    Fix lint errors

--- cpp_pubsub/src/publisher_member_function.cpp
+++ cpp_pubsub/src/publisher_member_function.cpp
@@ -1,26 +1,22 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/**
+ * @file publisher_member_function.cpp
+ * @author Akash Ravindra (aravind2@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <random>
 #include <string>
 
+#include "custom_interfaces/srv/change_string.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "custom_interfaces/srv/change_string.hpp"
 
 using namespace std::chrono_literals;
 
@@ -33,8 +29,11 @@ class MinimalPublisher : public rclcpp::Node {
       : Node("Custom_Node_Publisher"), msg_("The power-ball number is = ") {
     publisher_ =
         this->create_publisher<std_msgs::msg::String>("custom_topic", 10);
-    service_ = this->create_service<custom_interfaces::srv::ChangeString>("change_string", std::bind(&MinimalPublisher::change_string_callback, this, std::placeholders::_1, std::placeholders::_2));
-    if(msg_ == "The power-ball number is = "){
+    service_ = this->create_service<custom_interfaces::srv::ChangeString>(
+        "change_string",
+        std::bind(&MinimalPublisher::change_string_callback, this,
+                  std::placeholders::_1, std::placeholders::_2));
+    if (msg_ == "The power-ball number is = ") {
       RCLCPP_WARN_STREAM(rclcpp::get_logger("rclcpp"), "Using Default String");
     }
     timer_ = this->create_wall_timer(
@@ -44,18 +43,21 @@ class MinimalPublisher : public rclcpp::Node {
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    int random_number = rand() % 100;
+    unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+    int random_number = rand_r(&seed) % 100;
     message.data = msg_ + std::to_string(random_number);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    RCLCPP_INFO(this->get_logger(), "Publishing: " + message.data);
     publisher_->publish(message);
   }
-  void change_string_callback(const std::shared_ptr<custom_interfaces::srv::ChangeString::Request> request,
-                        std::shared_ptr<custom_interfaces::srv::ChangeString::Response> response)
-  {
+  void change_string_callback(
+      const std::shared_ptr<custom_interfaces::srv::ChangeString::Request>
+          request,
+      std::shared_ptr<custom_interfaces::srv::ChangeString::Response>
+          response) {
     RCLCPP_DEBUG_STREAM(this->get_logger(), "Service Request Received");
     msg_ = request->input;
     response->success = true;
-    RCLCPP_INFO_STREAM(this->get_logger(), ("Changing the string to: "+msg_));
+    RCLCPP_INFO_STREAM(this->get_logger(), ("Changing the string to: " + msg_));
   }
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
@@ -63,7 +65,7 @@ class MinimalPublisher : public rclcpp::Node {
   std::string msg_;
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
--- cpp_pubsub/src/update_publisher.cpp
+++ cpp_pubsub/src/update_publisher.cpp
@@ -1,46 +1,65 @@
-#include "rclcpp/rclcpp.hpp"
-#include "custom_interfaces/srv/change_string.hpp"
-
+/**
+ * @file update_publisher.cpp
+ * @author Akash Ravindra (aravind2@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <chrono>
 #include <cstdlib>
 #include <memory>
 
+#include "custom_interfaces/srv/change_string.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 using namespace std::chrono_literals;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
-    std::string update_request = "";
+  std::string update_request = "";
   if (argc == 1) {
-      RCLCPP_ERROR_STREAM(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
-      return 1;
+    RCLCPP_ERROR_STREAM(rclcpp::get_logger("rclcpp"),
+                        "usage: add_two_ints_client X Y");
+    return 1;
+  } else {
+    for (int i = 1; i < argc; i++) {
+      update_request.append(argv[i]);
+      update_request.append(" ");
+    }
   }
-  else{
-    for(int i=1; i<argc; i++){update_request.append(argv[i]);update_request.append(" ");}
-  }
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("change_string_client");
+  std::shared_ptr<rclcpp::Node> node =
+      rclcpp::Node::make_shared("change_string_client");
   rclcpp::Client<custom_interfaces::srv::ChangeString>::SharedPtr client =
-    node->create_client<custom_interfaces::srv::ChangeString>("change_string");
+      node->create_client<custom_interfaces::srv::ChangeString>(
+          "change_string");
 
-  auto request = std::make_shared<custom_interfaces::srv::ChangeString::Request>();
+  auto request =
+      std::make_shared<custom_interfaces::srv::ChangeString::Request>();
   request->input = update_request;
 
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
-      RCLCPP_FATAL_STREAM(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      RCLCPP_FATAL_STREAM(
+          rclcpp::get_logger("rclcpp"),
+          "Interrupted while waiting for the service. Exiting.");
       return 0;
     }
-    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"),
+                       "service not available, waiting again...");
   }
 
   auto result = client->async_send_request(request);
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
-  {
-    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), ("Result : " + std::to_string(result.get()->success)));
+      rclcpp::FutureReturnCode::SUCCESS) {
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"),
+                       ("Result : " + std::to_string(result.get()->success)));
   } else {
-    RCLCPP_ERROR_STREAM(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+    RCLCPP_ERROR_STREAM(rclcpp::get_logger("rclcpp"),
+                        "Failed to call service add_two_ints");
   }
 
   rclcpp::shutdown();
