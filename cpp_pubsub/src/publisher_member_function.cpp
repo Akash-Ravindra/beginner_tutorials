52abb21d162ef35b2f55af14f4e052ba0ffcfdbd
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-11-16 19:52:03 -0500

    Added license

--- cpp_pubsub/src/publisher_member_function.cpp
+++ cpp_pubsub/src/publisher_member_function.cpp
@@ -1,12 +1,31 @@
 /**
  * @file publisher_member_function.cpp
  * @author Akash Ravindra (aravind2@umd.edu)
- * @brief
+ * @brief 
  * @version 0.1
  * @date 2022-11-16
- *
- * @copyright Copyright (c) 2022
- *
+ * 
+ * MIT License
+ * Copyright (c) 2022 Akash Ravindra
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
  */
 #include <chrono>
 #include <functional>
@@ -45,7 +64,7 @@ class MinimalPublisher : public rclcpp::Node {
         std::bind(&MinimalPublisher::change_string_callback, this,
                   std::placeholders::_1, std::placeholders::_2));
     if (msg_ == "The power-ball number is = ") {
-      RCLCPP_WARN_STREAM(rclcpp::get_logger("rclcpp"), "Using Default String");
+      RCLCPP_WARN_STREAM(this->get_logger(), "Using Default String");
     }
     // Create a timer
     timer_ = this->create_wall_timer(
