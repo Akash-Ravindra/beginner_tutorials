ff45f6e14cd4a51972c94dbb0f61414a6d1f7045
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-11-16 19:31:52 -0500

    Added comments

--- cpp_pubsub/src/subscriber_member_function.cpp
+++ cpp_pubsub/src/subscriber_member_function.cpp
@@ -1,17 +1,13 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/**
+ * @file subscriber_member_function.cpp
+ * @author Akash Ravindra (aravind2@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <functional>
 #include <memory>
 
@@ -19,16 +15,22 @@
 #include "std_msgs/msg/string.hpp"
 
 using std::placeholders::_1;
-
+/// @brief This class is used to subscribe to a topic and print the message
+/// The class inherits from the rclcpp::Node class
+/// The class has a subscription member function which subscribes to a topic
+/// and prints the message
 class MinimalSubscriber : public rclcpp::Node {
  public:
   MinimalSubscriber() : Node("custom_node_subscriber") {
+    // Create a subscription
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "custom_topic", 10,
         std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
  private:
+  /// @brief This function is used to print the message received from the
+  /// @param msg
   void topic_callback(const std_msgs::msg::String& msg) const {
     RCLCPP_INFO(this->get_logger(), "The publisher said -> '%s'",
                 msg.data.c_str());
@@ -36,6 +38,10 @@ class MinimalSubscriber : public rclcpp::Node {
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
+/// @brief This function is used to create a subscriber node and spin it
+/// @param argc
+/// @param argv
+/// @return
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
